4528d7d49812f783a4c9c183799fe61a30353bab
Mel Massadian <melmassadian@gmail.com>
2021-03-15 06:18:05 +0100

    fix(WIP): ⚡️ Removing embeded unity resources
    Build fails because I've change the hierarchy.
    I have already started the packaged version of it anyway..

--- build_osx.sh
+++ build_osx.sh
@@ -1,4 +1,5 @@
 set -e
+mkdir -p build
 cd build
-cmake -DCMAKE_OSX_SYSROOT=/Developer/OSX_sdks/MacOSX10.6.sdk -DCMAKE_OSX_ARCHITECTURES="x86_64" -DDAZ_STUDIO_EXE_DIR="/Users/imac/Daz 3D/Applications/DAZ 3D/DAZStudio4 64-bit" ..
+cmake -DCMAKE_OSX_SYSROOT=/Developer/OSX_sdks/MacOSX10.6.sdk -DCMAKE_OSX_ARCHITECTURES="x86_64" -DDAZ_STUDIO_EXE_DIR="/Users/$(whoami)/Daz 3D/Applications/DAZ 3D/DAZStudio4 64-bit" ..
 make
