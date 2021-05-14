74e94374bc6c935b30dccefbfc0a3c02a041ee3e
Mel Massadian <melmassadian@gmail.com>
2021-05-15 00:31:22 +0200

    feat: 🚚 Merge interesting upstream changes

--- Common/DzRuntimePluginAction.h
+++ Common/DzRuntimePluginAction.h
@@ -6,37 +6,59 @@
 #include "QtCore/qfile.h"
 #include "QtCore/qtextstream.h"
 
-class DzRuntimePluginAction : public DzAction {
-	 Q_OBJECT
-public:
+// Struct to remember attachment info
+struct AttachmentInfo
+{
+	DzNode *Parent;
+	DzNode *Child;
+};
 
-	 DzRuntimePluginAction(const QString& text = QString::null, const QString& desc = QString::null);
-	 virtual ~DzRuntimePluginAction();
+class DzRuntimePluginAction : public DzAction
+{
+	Q_OBJECT
+public:
+	DzRuntimePluginAction(const QString &text = QString::null, const QString &desc = QString::null);
+	virtual ~DzRuntimePluginAction();
 
 protected:
-	 QString CharacterName;
-	 QString ImportFolder;
-	 QString CharacterFolder;
-	 QString CharacterFBX;
-	 QString AssetType;
-	 QString MorphString;
-	 QString FBXVersion;
-	 QMap<QString,QString> MorphMapping;
+	QString CharacterName;
+	QString ImportFolder;
+	QString CharacterFolder;
+	QString CharacterFBX;
+	QString AssetType;
+	QString MorphString;
+	QString FBXVersion;
+	QMap<QString, QString> MorphMapping;
+	QList<QString> PoseList;
 
-	 bool ExportMorphs;
-	 bool ExportSubdivisions;
-	 bool ShowFbxDialog;
-	 DzNode* Selection;
+	bool ExportMorphs;
+	bool ExportSubdivisions;
+	bool ShowFbxDialog;
+	bool ExportMaterialPropertiesCSV;
+	DzNode *Selection;
 
-	 virtual QString getActionGroup() const { return tr("Bridges"); }
-	 virtual QString getDefaultMenuPath() const { return tr("&File/Send To"); }
+	virtual QString getActionGroup() const { return tr("Bridges"); }
+	virtual QString getDefaultMenuPath() const { return tr("&File/Send To"); }
 
-	 virtual void Export();
+	virtual void Export();
+	virtual void ExportNode(DzNode *Node);
 
-	 virtual void WriteConfiguration() = 0;
-	 virtual void SetExportOptions(DzFileIOSettings &ExportOptions) = 0;
+	virtual void WriteConfiguration() = 0;
+	virtual void SetExportOptions(DzFileIOSettings &ExportOptions) = 0;
 
-	 // Need to temporarily rename surfaces if there is a name collision
-	 void RenameDuplicateMaterials(DzNode* Node, QList<QString>& MaterialNames, QMap<DzMaterial*, QString>& OriginalMaterialNames);
-	 void UndoRenameDuplicateMaterials(DzNode* Node, QList<QString>& MaterialNames, QMap<DzMaterial*, QString>& OriginalMaterialNames);
+	// Need to temporarily rename surfaces if there is a name collision
+	void RenameDuplicateMaterials(DzNode *Node, QList<QString> &MaterialNames, QMap<DzMaterial *, QString> &OriginalMaterialNames);
+	void UndoRenameDuplicateMaterials(DzNode *Node, QList<QString> &MaterialNames, QMap<DzMaterial *, QString> &OriginalMaterialNames);
+
+	// Used to find all the unique props in a scene for Environment export
+	void GetScenePropList(DzNode *Node, QMap<QString, DzNode *> &Types);
+
+	// During Environment export props need to get disconnected as they are exported.
+	void DisconnectNode(DzNode *Node, QList<AttachmentInfo> &AttachmentList);
+	void ReconnectNodes(QList<AttachmentInfo> &AttachmentList);
+
+	// During Skeletal Mesh Export Disconnect Override Controllers
+	QList<QString> DisconnectOverrideControllers();
+	void ReconnectOverrideControllers(QList<QString> &DisconnetedControllers);
+	QList<QString> ControllersToDisconnect;
 };
