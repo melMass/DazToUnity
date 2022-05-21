e9fc4820efd0c85ff6506cc2217eb4053f6bfaf9
mel <mel@melmassadian.com>
2022-05-21 20:18:01 +0200

    feat: 🎨 better powershell script for build

--- build.ps1
+++ build.ps1
@@ -1,14 +1,62 @@
+[CmdletBinding()]
+	param(
+    [Parameter(
+    HelpMessage="Builds a release build")]
+    [switch]
+    [Alias("r")]
+    $Release,
+    [Parameter(
+      HelpMessage="The path to your DAZ Studio's installation directory")]
+    [Alias("dz")]
+    $DazStudioPath = "C:\Daz 3D\Applications\64-bit\DAZ 3D\DAZStudio4\",
+    [Parameter(
+      HelpMessage="Disable installation of the plugin"
+    )]
+    [switch]
+    $NoInstall
+	)
+
+
+# - CHECKS
+
+# - daz studio path exists
+if (-Not (Test-Path $DazStudioPath) -And -Not $NoInstall){
+  Write-Host "The path to your DAZ Studio's installation directory is invalid" -ForegroundColor Red
+  Write-Host "You can bypass installation by using -NoInstall" -ForegroundColor Yellow
+  exit
+}
+
+# - cmake is available in PATH
+if ( -Not (get-command cmake -ErrorAction Ignore)) {
+  Write-Host "CMake is not installed. Please install CMake before running this script." -ForegroundColor Red
+  exit 1
+}
+
+# - gather info
 $current_pwd = $PWD
-
 $parent_dir = Split-Path $MyInvocation.MyCommand.Path
-
 Set-Location $parent_dir
 
+# - build config type
+$config = "Debug"
+if ($Release){
+  $config = "Release"
+  Write-Host "Building a release build" -ForegroundColor Green
+}
+else{
+  Write-Host "Building a debug build" -ForegroundColor Yellow
+}
+
+# - build in 'build' directory
 New-Item -ErrorAction Ignore -ItemType Directory build
 Set-Location build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DDAZ_STUDIO_EXE_DIR="C:\Daz 3D\Applications\64-bit\DAZ 3D\DAZStudio4\"
-cmake --build .
-cmake --install .
+cmake .. -DDAZ_STUDIO_EXE_DIR="$DazStudioPath"
+cmake --build . --config $config
+
+if (-Not $NoInstall){
+  cmake --install .
+}
+
 
 
 Set-Location $current_pwd
